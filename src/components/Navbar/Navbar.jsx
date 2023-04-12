7099da0e2646189626f37726c84e6e6b7ee97b39
shaaheensaab <shaaheensaab@gmail.email>
2023-04-12 14:01:42 +0600

    Navlink active-state implemented

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,15 +6,21 @@ import Button from '../Button/Button';
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
+    const [active, setActive] = useState('');
+
+    const handleActive = (state) => {
+        setActive(state)
+    }
+
     return (
         <nav className='w-[80%] mx-auto flex flex-row-reverse md:flex-row justify-between lg:text-lg uppercase py-5 items-center border-b pb-3'>
             <Link to='/'><h1 className='text-2xl font-extrabold'>Exxistence</h1></Link>
 
             <ul className={`absolute z-10 flex flex-col md:flex-row ${open ? 'top-16 left-[10%] bg-white border-t space-y-3 md:space-y-0 pr-3 pb-10 pt-2 ' : 'top-16 -left-36'} duration-300 md:static md:space-x-3 text-sm font-extrabold`}>
-                <Link to='/'>Home</Link>
-                <Link to='/statistics'>Statistics</Link>
-                <Link to='/applied-jobs'>Applied Jobs</Link>
-                <Link to='/blog'>Blog</Link>
+                <Link className={active === 'Home' ? 'border-b-2 border-black' : ''} onClick={() => handleActive('Home')} to='/'>Home</Link>
+                <Link className={active === 'Statistics' ? 'border-b-2 border-black' : ''} onClick={() => handleActive('Statistics')} to='/statistics'>Statistics</Link>
+                <Link className={active === 'Applied Jobs' ? 'border-b-2 border-black' : ''} onClick={() => handleActive('Applied Jobs')} to='/applied-jobs'>Applied Jobs</Link>
+                <Link className={active === 'Blog' ? 'border-b-2 border-black' : ''} onClick={() => handleActive('Blog')} to='/blog'>Blog</Link>
             </ul>
             <div className='hidden md:block'>
                 <div className='flex gap-2'>
